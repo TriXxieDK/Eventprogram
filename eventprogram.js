72b6f7040107fc7ef83cea95f0ed0965d74b0b2e
Andreas Heglingegård <andreas_heglingegaard@hotmail.com>
2020-07-24 00:01:46 +0200

    Update eventprogram.js
    Update version

--- eventprogram.js
+++ eventprogram.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Eventprogram
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @updateURL    https://github.com/TriXxieDK/Eventprogram/blob/master/eventprogram.js
